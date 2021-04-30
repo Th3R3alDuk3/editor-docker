d8064379bb93a10df39200897577b449d1d5ec1a
Th3R3alDuk3 <60510941+Th3R3alDuk3@users.noreply.github.com>
2021-04-30 14:47:38 +0200

    add gui

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const expressWs = require('express-ws');
 
-// const exec = require('child_process');
+const app = express();
+expressWs(app);
 
-/* static files */
+/**/
+
+// redirect static files
 app.use(express.static('public'));
 app.use("/node_modules", express.static('node_modules'));
 
+// support json- and url-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/**/
+
+// spawn child process
+const {spawn} = require('child_process');
+
+app.ws('/', (ws, req) => {
+
+  ws.on('message', msg => {
+    
+    console.log(msg);
+
+    let process = spawn(
+      'gcc', 
+      ['-v']
+    );
+
+    process.stdout.on('data', (data) => { ws.send(`${data}`); });  
+    process.stderr.on('data', (data) => { ws.send(`${data}`); }); 
+
+    process.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
+  });
+
+  ws.on('close', () => {
+      console.log('websocket disconnected ...');
+  });
+
+  req.end();
+
+});
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
-})
+  res.sendFile('index.html')
+});
 
 app.listen(8080, () => {
-  console.log('app listening at http://localhost:8080')
-})
+  console.log('http://localhost:8080')
+});
